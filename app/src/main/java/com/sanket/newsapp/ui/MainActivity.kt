2a720d92745c148280d69814e794438996130aa4
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-06-10 19:46:14 +0530

    7: Added NewsListFragment and moved the rendering of news list into the fragment

--- app/src/main/java/com/sanket/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/sanket/newsapp/ui/MainActivity.kt
@@ -15,41 +15,19 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
-    private val articles by lazy { mutableListOf<Article>() }
-    private val adapter by lazy { NewsAdapter(articles) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initLD()
-        initRecyclerView()
+        initViews()
         viewModel.getNews()
     }
 
-    private fun initRecyclerView() {
-        binding.apply {
-            rvNews.apply {
-                adapter = this@MainActivity.adapter
-                layoutManager = LinearLayoutManager(this@MainActivity)
-            }
-        }
+    private fun initViews() {
+
     }
 
-    private fun initLD() {
-        viewModel.newsLD.observe(this) {
-            when (it.status) {
-                Status.LOADING -> binding.progress.isVisible = true
-                Status.ERROR -> {
-                    binding.progress.isVisible = false
-                    Toast.makeText(this, it.message?.getText(this), Toast.LENGTH_SHORT).show()
-                }
-                Status.SUCCESS -> {
-                    binding.progress.isVisible = false
-//                    Toast.makeText(this, it.data.toString(), Toast.LENGTH_SHORT).show()
-                    adapter.addData(it.data?.articles)
-                }
-            }
-        }
-    }
+
 }
---
+++ app/src/main/java/com/sanket/newsapp/ui/NewsListFragment.kt
@@ -0,0 +1,83 @@
+package com.sanket.newsapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sanket.newsapp.MainViewModel
+import com.sanket.newsapp.api.Status
+import com.sanket.newsapp.api.models.Article
+import com.sanket.newsapp.databinding.FragmentNewsListBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [NewsListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class NewsListFragment : Fragment() {
+
+    private lateinit var binding: FragmentNewsListBinding
+    private val articles by lazy { mutableListOf<Article>() }
+    private val adapter by lazy { NewsAdapter(articles) }
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentNewsListBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initLD()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.apply {
+            rvNews.apply {
+                adapter = this@NewsListFragment.adapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+        }
+    }
+
+    private fun initLD() {
+        viewModel.newsLD.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Status.LOADING -> binding.progress.isVisible = true
+                Status.ERROR -> {
+                    binding.progress.isVisible = false
+                    Toast.makeText(requireContext(), it.message?.getText(requireContext()), Toast.LENGTH_SHORT).show()
+                }
+                Status.SUCCESS -> {
+                    binding.progress.isVisible = false
+                    adapter.addData(it.data?.articles)
+                }
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment NewsListFragment.
+         */
+        @JvmStatic
+        fun newInstance() = NewsListFragment()
+    }
+}
